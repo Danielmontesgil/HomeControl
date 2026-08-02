f2d30da52d4bd9e568fd331fe6847b45c8e4294b
Daniel Montes Gil <dmontesgil@gmail.com>
2026-08-02 19:17:24 +0200

    Update BuildInfo.h

--- include/Core/BuildInfo.h
+++ include/Core/BuildInfo.h
@@ -1,4 +1,4 @@
 #pragma once
 #define BUILD_VERSION "0.0.1"
-#define BUILD_NUMBER "15"
-#define BUILD_TIMESTAMP "2026-07-31 12:47:48"
+#define BUILD_NUMBER "68"
+#define BUILD_TIMESTAMP "2026-08-02 18:41:39"
